8ac9aeabab59e840d19145b091f22be36dc0b16f
CG379 <cgregor379@gmail.com>
2025-02-22 09:28:27 +1100

    chat functions

---
+++ utils/chat_funs.py
@@ -0,0 +1,34 @@
+import tiktoken
+import streamlit as st
+from utils.api_funs import send_api_request, execute_fun
+
+def run_chat(messages, funs):
+    if "live_chat" not in st.session_state:
+        st.session_state["live_chat"] = [{"role": "system", "content": "Hello! How can I assist you?"}]
+    internal_chat_history = st.session_state["live_chat"].copy()
+    chat_response = send_api_request(messages, funs)
+    system_message = chat_response.json()["choices"][0]["message"]
+
+    if system_message["role"] == "system":
+        internal_chat_history.append({"role": "system", "content": system_message})
+
+
+
+
+
+def clear_chat_history():
+    """ Clear the chat history stored in the Streamlit session state """
+    del st.session_state["live_chat_history"]
+    del st.session_state["full_chat_history"]
+    del st.session_state["api_chat_history"]
+
+def count_tokens(text):
+    """ to make sure I don't go over free limit"""
+    # In case this breaks
+    if not isinstance(text, str):
+        return 0
+    # TODO: make model a global var or env file var or something
+    encoding = tiktoken.encode_for_model(AI_MODEL)
+    total_tokens = len(encoding.encode(text))
+
+    return total_tokens
